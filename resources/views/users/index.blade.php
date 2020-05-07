174115c689654fb95496ffc055f95de3f43e2c87
Yessi Munguengui <yessimunguengui@gmail.com>
2020-05-07 14:43:03 +0200

    index

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-md-12">
             <br />
-            <h3 align="center">Users Data</h3>
+            <h3 align="center">Student Data</h3>
             <br />
             @if($message = Session::get('success'))
                 <div class="alert alert-success">
@@ -13,7 +13,7 @@
                 </div>
             @endif
             <div align="right">
-                <a href="{{route('users.create')}}" class="btn btn-primary">Add</a>
+                <a href="{{route('student.create')}}" class="btn btn-primary">Add</a>
                 <br />
                 <br />
             </div>
@@ -21,24 +21,26 @@
                 <tr>
                     <th>First Name</th>
                     <th>Last Name</th>
-                    <th>Mail</th>
                     <th>Edit</th>
                     <th>Delete</th>
                 </tr>
-                @foreach($users as $row)
+                @foreach($students as $row)
                     <tr>
                         <td>{{$row['name']}}</td>
                         <td>{{$row['surname']}}</td>
-                        <td>{{$row['email']}}</td>
-                        <td><a href="{{action('UserController@edit', $row['id'])}}" class="btn btn-warning">Edit</a></td>
-                        <td></td>
+                        <td><a href="{{action('Controller@edit', $row['id'])}}" class="btn btn-warning">Edit</a></td>
+                        <td>
+                            <form method="post" class="delete_form" action="{{action('StudentController@destroy', $row['id'])}}">
+                                {{csrf_field()}}
+                                <input type="hidden" name="_method" value="DELETE" />
+                                <button type="submit" class="btn btn-danger">Delete</button>
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </table>
         </div>
     </div>
-
-
     <script>
         $(document).ready(function(){
             $('.delete_form').on('submit', function(){
